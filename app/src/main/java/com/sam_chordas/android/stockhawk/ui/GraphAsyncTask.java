d93d571077cccdf9fa420454da590c0ea1555d1b
genericjohndoe <j.robinson.johnson21@gmail.com>
2017-07-04 00:01:03 -0500

    replaced yahoo historical stock data api with alpha vantage api

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/GraphAsyncTask.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/GraphAsyncTask.java
@@ -3,16 +3,13 @@ package com.sam_chordas.android.stockhawk.ui;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -23,7 +20,6 @@ import okhttp3.Response;
  */
 
 public class GraphAsyncTask extends AsyncTask<String, Void, Void> {
-    String[] dates = new String[2];
 
     GraphLoadedCallback graphLoadedCallback;
 
@@ -31,58 +27,37 @@ public class GraphAsyncTask extends AsyncTask<String, Void, Void> {
         this.graphLoadedCallback = graphLoadedCallback;
     }
 
-    private Void getStockInfoFromJSON(String JsonString)
+    private void getStockInfoFromJSON(String JsonString)
             throws JSONException {
         MyStocksActivity.StockData = new ArrayList<>();
         JSONObject Json = new JSONObject(JsonString);
-        JSONObject query = Json.getJSONObject("query");
-        JSONObject results = query.getJSONObject("results");
-        JSONArray StockInfo = results.getJSONArray("quote");
-
-        for (int i = 0; i < StockInfo.length(); i++) {
+        JSONObject StockInfo = Json.getJSONObject("Time Series (Daily)");
+        Iterator x = StockInfo.keys();
+        while (x.hasNext()){
             StockData datum = new StockData();
-            JSONObject stockPrice = StockInfo.getJSONObject(i);
-            String[] stringDate = stockPrice.getString("Date").split("-");
+            String key = (String) x.next();
+            String[] stringDate = key.split("-");
             Calendar cal = Calendar.getInstance();
             cal.set(Integer.parseInt(stringDate[0]),
-            Integer.parseInt(stringDate[1]), Integer.parseInt(stringDate[2]));
+                    Integer.parseInt(stringDate[1]), Integer.parseInt(stringDate[2].substring(0, 2)));
+            Log.i("cal", cal.toString());
             datum.date = cal.getTimeInMillis();
-            datum.price = stockPrice.getDouble("Close");
-            datum.CalDate = stockPrice.getString("Date");
+            JSONObject object = (JSONObject) StockInfo.get(key);
+            datum.price = object.getDouble("4. close");
+            datum.CalDate = key.split(" ")[0];
             MyStocksActivity.StockData.add(datum);
-
         }
-        return null;
     }
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        Calendar calendar = Calendar.getInstance();
-        Calendar calendar2 = Calendar.getInstance();
-        calendar2.add(Calendar.DATE, -7);
-        dates[1] = new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime());
-        dates[0] = new SimpleDateFormat("yyyy-MM-dd").format(calendar2.getTime());
-
-    }
 
     @Override
     protected Void doInBackground(String... strings) {
+        Log.i("MSA", "doInBackground");
         OkHttpClient client = new OkHttpClient();
 
-        StringBuilder urlStringBuilder = new StringBuilder();
-        try{
-            // Base URL for the Yahoo query
-            urlStringBuilder.append("https://query.yahooapis.com/v1/public/yql?q=");
-            urlStringBuilder.append(URLEncoder.encode("select Date, Close from yahoo.finance.historicaldata where symbol "
-                    + "= \""+strings[0]+"\" and startDate = \""+ dates[0]+"\" and endDate = \"" + dates[1] + "\"", "UTF-8"));
-            urlStringBuilder.append("&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables."
-                    + "org%2Falltableswithkeys&callback=");
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        String url = urlStringBuilder.toString();
+        String url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + strings[0] +
+                "&outputsize=compact&apikey=IYFRSS5SYRI6OTEP";
+        Log.i("url", url);
 
         Request request = new Request.Builder()
                 .url(url)
